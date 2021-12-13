3d46f141df3a9d3b3ca0aa3d0cee3131ffd074dd
Reja <rezanurrochmat@gmail.com>
2021-12-13 20:32:29 +0700

    Add kotlin main

---
+++ src/main/kotlin/Arithmetic.kt
@@ -0,0 +1,9 @@
+class Arithmetic {
+    fun addition(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+
+    fun division(num1: Int, num2: Int): Int {
+        return num1 - num2
+    }
+}
